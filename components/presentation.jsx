f514e94f34e7a1d950fd480fd952590e714c1221
Mohamed Khalef <108290860+khalefmed@users.noreply.github.com>
2024-03-12 14:56:59 +0000

    Update presentation.jsx

--- components/presentation.jsx
+++ components/presentation.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { motion, useAnimation } from 'framer-motion';
+import Image from 'next/image';
 
 export default function Presentation(props){
     return (
@@ -19,18 +20,18 @@ export default function Presentation(props){
                 opacity : 1,
                 transition : {duration : 2}
             }}
-            className=" lg:px-20 md:px-16 xs:px-8 w-[100%] h-fit flex lg:flex-row xs:flex-col justify-center  align-center gap-12  ">
+            className=" lg:px-20 md:px-16 xs:px-8 w-[100%] h-fit flex lg:flex-row xs:flex-col justify-center items-center  align-center gap-12  ">
                 <div
                 
                  className=" flex flex-col text-center gap-6 lg:w-[60%] xs:w-full">
                     <h1 className={`section-title text-center lg:text-4xl md:text-3xl xs:text-2xl text-primaryColor font-bold mt-12  uppercase ${props.isArabic ? '' : 'tracking-[1rem] xs:tracking-[0.5rem]'} ` }>About</h1>
                     <p
-                    className=' lg:text-lg xs:text-md text-black  lg:leading-[2.5] xs:leading-[2.5]  text-center ' >{props.content}</p>
+                    className=' lg:text-lg xs:text-md text-black  lg:leading-[2.5] xs:leading-[2.5]  text-justify ' >{props.content}</p>
                     
                 </div>
-                {/* <div className=' flex justify-center align-center h-[100%] lg:pt-28   '>
-                    <img src="/sante.webp" alt="" className=" w-96   rounded-lg  shadow-2xl" />
-                </div> */}
+                <div className=' flex justify-center align-center h-[100%] lg:pt-28   '>
+                    <Image width={200} height={170} quality={100} src="/Tourad_image.jpg" alt="" className=" border-4 border-primaryColor  rounded-full  shadow-2xl" />
+                </div>
 
             </motion.div>
         </section>
