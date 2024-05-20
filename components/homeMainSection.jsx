7da684adfa57d8320539d4121c57db2df6880674
Mohamed Khalef <108290860+khalefmed@users.noreply.github.com>
2024-05-20 22:02:09 +0000

    Update homeMainSection.jsx

--- components/homeMainSection.jsx
+++ components/homeMainSection.jsx
@@ -7,11 +7,11 @@ export default function HomeMainSection(props){
             <div className=" flex justify-center align-start flex-col gap-6 mt-28">
                 <h1 className=" transition animate-homeAnimation delay-75 duration-700 font-bold lg:text-5xl md:text-4xl xs:text-3xl ">{props.chn}</h1>
                 <p className=" transition animate-homeAnimation delay-300 duration-700  lg:text-2xl md:text-xl xs:text-lg font-regular">{props.slogan}</p>
-                <div className=' flex md:justify-start xs:justify-center xs:align-center'>
+{/*                 <div className=' flex md:justify-start xs:justify-center xs:align-center'>
                     <Link href='/#' className=' animate-homeAnimation delay-300   border border-white border-1 rounded-full w-fit px-4 py-2 hover:bg-white hover:text-black transition duration-700'>
                         {props.moreInfos}
                     </Link>
-                </div>
+                </div> */}
             </div>
 
         </main>
